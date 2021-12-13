c9ee7a178d4b27993550bc4834fd760fbe9c1f0d
Sebastian Flor <sebastian.flor@toq.io>
2021-12-13 18:39:32 +0100

    feat(compile): Add compile script
    Add compilation script to export bytecode & ABI of Inbox contract

--- compile.js
+++ compile.js
@@ -1,1 +1,8 @@
-// compile code will go here
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+module.export = solc.compile(source, 1).contracts[':Inbox'];
