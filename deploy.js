3c003ee82081ac3b2cf729c897e948609f1b5ff7
Sebastian Flor <sebastian.flor@toq.io>
2021-12-17 17:28:13 +0100

    refactor(): Update dependencies
    Update dependencies and redeploy smart contract with new compiler version

--- deploy.js
+++ deploy.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+const { abi, evm } = require('./compile');
 
 const result = dotenv.config();
 
@@ -9,8 +9,17 @@ if (result.error) {
   throw result.error;
 }
 
-const RINKEBY_API = `https://rinkeby.infura.io/v3/${process.env.RINKEBY_API_KEY}`;
-const provider = new HDWalletProvider(process.env.PRIVATE_KEY, RINKEBY_API);
+/**
+ * BLOCKCHAIN TESTNET NODE
+ */
+const RINKEBY_NODE = `https://rinkeby.infura.io/v3/${process.env.RINKEBY_API_KEY}`;
+/**
+ * PROVIDER
+ */
+const provider = new HDWalletProvider({ mnemonic: process.env.PRIVATE_KEY, providerOrUrl: RINKEBY_NODE });
+/**
+ * WEB 3
+ */
 const web3 = new Web3(provider);
 
 (async () => {
@@ -21,12 +30,13 @@ const web3 = new Web3(provider);
    */
   console.log(`account: ${accounts[0]}`)
 
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there'] })
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there'] })
     .send({ gas: '1000000', from: accounts[0] });
 
   /**
    * contract deployed to 0xFB1287aFe8cb16e3b56053c55411EFdC0eEE4ade
+   * new address: 0xDA4968D7a9a4eCeBfa47eD6Ef1F763b7459F86C5
    */
   console.log(`contract deployed to ${result.options.address}`);
 
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const { interface, bytecode } = require('../compile');
+const { abi, evm } = require('../compile');
 
 const web3 = new Web3(ganache.provider());
 
@@ -17,8 +17,8 @@ beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
 
     // USE ONE OF THOSE ACCOUNTS TO DEPLOY THE CONTRACT
-    inboxContract = await new web3.eth.Contract(JSON.parse(interface))
-      .deploy({ data: bytecode, arguments: [initialMessage] })
+    inboxContract = await new web3.eth.Contract(abi)
+      .deploy({ data: evm.bytecode.object, arguments: [initialMessage] })
       .send({ from: accounts[0], gas: '1000000' });
   } catch (e) {
     console.log(e)
